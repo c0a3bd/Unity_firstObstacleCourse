7b3228ea7fcb3fcdfd114a8cc15ca828f2889080
c0a3bd <c0a3bd>
2022-06-09 16:24:27 -0400

    SerializeField completed

--- Assets/Scripts/mover.cs
+++ Assets/Scripts/mover.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class mover : MonoBehaviour
 {
-    float xValue = 0.01f;
-    float yValue = 0.01f;
-    int zValue = 0;
+    [SerializeField] float xValue = 0;
+    [SerializeField]  float yValue = 0.01f;
+    [SerializeField] int zValue = 0;
 
     // Start is called before the first frame update
     void Start()
